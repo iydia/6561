42b1640315e2093c3420304e681f3d19f21c391b
Lydia He <75390984+iydia@users.noreply.github.com>
2024-01-10 09:51:16 -0500

    Added abstraction for efficiency

--- game.js
+++ game.js
@@ -32,21 +32,20 @@ document.addEventListener('DOMContentLoaded', () =>  {
     function moveLeft() {
         for (let i = 0; i < 16; i++) {
           if (i % 4 === 0) {
-            let totalOne = tiles[i].innerHTML;
-            let totalTwo = tiles[i+1].innerHTML;
-            let totalThree = tiles[i+2].innerHTML;
-            let totalFour = tiles[i+3].innerHTML;
-            let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)];
+            let sum1 = tiles[i].innerHTML;
+            let sum2 = tiles[i+1].innerHTML;
+            let sum3 = tiles[i+2].innerHTML;
+            let sum4 = tiles[i+3].innerHTML;
+            let row = [sum1, sum2, sum3, sum4].map(num => +num);
     
             let filteredRow = row.filter(num => num);
-            let missing = 4 - filteredRow.length;
-            let numTiles = Array(missing).fill(0);
+            let remaining = 4 - filteredRow.length;
+            let numTiles = Array(remaining).fill(0);
             let newRow = filteredRow.concat(numTiles);
     
-            tiles[i].innerHTML = newRow[0];
-            tiles[i+1].innerHTML = newRow[1];
-            tiles[i+2].innerHTML = newRow[2];
-            tiles[i+3].innerHTML = newRow[3];
+            for (let j = 0; j < 4; j++) {
+              tiles[i + j].innerHTML = newRow[j];
+            }
           }
         }
       }
@@ -54,91 +53,75 @@ document.addEventListener('DOMContentLoaded', () =>  {
     function moveRight() {
       for (let i = 0; i < 16; i++) {
         if (i % 4 === 0) {
-          let totalOne = tiles[i].innerHTML;
-          let totalTwo = tiles[i+1].innerHTML;
-          let totalThree = tiles[i+2].innerHTML;
-          let totalFour = tiles[i+3].innerHTML;
-          let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)];
+          let sum1 = tiles[i].innerHTML;
+          let sum2 = tiles[i+1].innerHTML;
+          let sum3 = tiles[i+2].innerHTML;
+          let sum4 = tiles[i+3].innerHTML;
+          let row = [sum1, sum2, sum3, sum4].map(num => +num);
   
           let filteredRow = row.filter(num => num);
-          let missing = 4 - filteredRow.length;
-          let numTiles = Array(missing).fill(0);
+          let remaining = 4 - filteredRow.length;
+          let numTiles = Array(remaining).fill(0);
           let newRow = numTiles.concat(filteredRow);
   
-          tiles[i].innerHTML = newRow[0];
-          tiles[i+1].innerHTML = newRow[1];
-          tiles[i+2].innerHTML = newRow[2];
-          tiles[i+3].innerHTML = newRow[3];
+          for (let j = 0; j < 4; j++) {
+            tiles[i + j].innerHTML = newRow[j];
+          }
         }
       }
     }  
   
     function moveUp() {
       for (let i = 0; i < 4; i++) {
-        let totalOne = tiles[i].innerHTML;
-        let totalTwo = tiles[i+width].innerHTML;
-        let totalThree = tiles[i+(width*2)].innerHTML;
-        let totalFour = tiles[i+(width*3)].innerHTML;
-        let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)];
+        let sum1 = tiles[i].innerHTML;
+        let sum2 = tiles[i+width].innerHTML;
+        let sum3 = tiles[i+(width*2)].innerHTML;
+        let sum4 = tiles[i+(width*3)].innerHTML;
+        let column = [sum1, sum2, sum3, sum4].map(num => +num);
   
         let filteredColumn = column.filter(num => num);
-        let missing = 4 - filteredColumn.length;
-        let numTiles = Array(missing).fill(0);
+        let remaining = 4 - filteredColumn.length;
+        let numTiles = Array(remaining).fill(0);
         let newColumn = filteredColumn.concat(numTiles);
   
-        tiles[i].innerHTML = newColumn[0];
-        tiles[i+width].innerHTML = newColumn[1];
-        tiles[i+(width*2)].innerHTML = newColumn[2];
-        tiles[i+(width*3)].innerHTML = newColumn[3];
+        for (let j = 0; j < 4; j++) {
+          tiles[i + j * width].innerHTML = newColumn[j];
+        }
       }
     }
   
     function moveDown() {
       for (let i = 0; i < 4; i++) {
-        let totalOne = tiles[i].innerHTML;
-        let totalTwo = tiles[i+width].innerHTML;
-        let totalThree = tiles[i+(width*2)].innerHTML;
-        let totalFour = tiles[i+(width*3)].innerHTML;
-        let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)];
+        let sum1 = tiles[i].innerHTML;
+        let sum2 = tiles[i+width].innerHTML;
+        let sum3 = tiles[i+(width*2)].innerHTML;
+        let sum4 = tiles[i+(width*3)].innerHTML;
+        let column = [sum1, sum2, sum3, sum4].map(num => +num);
   
         let filteredColumn = column.filter(num => num);
-        let missing = 4 - filteredColumn.length;
-        let numTiles = Array(missing).fill(0);
+        let remaining = 4 - filteredColumn.length;
+        let numTiles = Array(remaining).fill(0);
         let newColumn = numTiles.concat(filteredColumn);
   
-        tiles[i].innerHTML = newColumn[0];
-        tiles[i+width].innerHTML = newColumn[1];
-        tiles[i+(width*2)].innerHTML = newColumn[2];
-        tiles[i+(width*3)].innerHTML = newColumn[3];
+        for (let j = 0; j < 4; j++) {
+          tiles[i + j * width].innerHTML = newColumn[j];
+        }
       }
     }
-  
-    function collapseRow() {
-        for (let i = 0; i < 15; i++) {
-          if (tiles[i].innerHTML === tiles[i + 1].innerHTML) {
-            let combinedTotal = 3 * parseInt(tiles[i].innerHTML);
-            tiles[i].innerHTML = combinedTotal;
-            tiles[i + 1].innerHTML = 0;
-            count += combinedTotal;
-            score.innerHTML = count;
-          }
+
+    function collapse(length, increment){
+      for(let i = 0; i < length; i++) {
+        if(tiles[i].innerHTML === tiles[i + increment].innerHTML) {
+          let combinedTotal = 3 * parseInt(tiles[i].innerHTML);
+          tiles[i].innerHTML = combinedTotal;
+          tiles[i + increment].innerHTML = 0;
+          count += combinedTotal;
+          score.innerHTML = count;
         }
-        isWin();
       }
-    
-      function collapseColumn() {
-        for (let i = 0; i < 12; i++) {
-          if (tiles[i].innerHTML === tiles[i + width].innerHTML) {
-            let combinedTotal = 3 * parseInt(tiles[i].innerHTML);
-            tiles[i].innerHTML = combinedTotal;
-            tiles[i + width].innerHTML = 0;
-            count += combinedTotal;
-            score.innerHTML = count;
-          }
-        }
-        isWin();
-      }
-    
+      isWin();
+    }
+      
       function isWin() {
         for (let i = 0; i < tiles.length; i++) {
           if (tiles[i].innerHTML == 6561) {
@@ -164,28 +147,28 @@ document.addEventListener('DOMContentLoaded', () =>  {
   
     function keyLeft() {
         moveLeft();
-        collapseRow();
+        collapse(15, 1);
         moveLeft();
         generate();
     }
   
     function keyRight() {
         moveRight();
-        collapseRow();
+        collapse(15, 1);
         moveRight();
         generate();
     }
   
     function keyUp() {
         moveUp();
-        collapseColumn();
+        collapse(12, width);
         moveUp();
         generate();
     }
   
     function keyDown() {
         moveDown();
-        collapseColumn();
+        collapse(12, width);
         moveDown();
         generate();
     }
@@ -209,25 +192,22 @@ document.addEventListener('DOMContentLoaded', () =>  {
             tiles[i].style.alignItems = 'center';
             tiles[i].style.justifyContent = 'center';
             tiles[i].style.fontSize = '';
+            tiles[i].style.color = '#546B51';
           if (tiles[i].innerHTML == 0) {
             tiles[i].style.backgroundColor = '#BDCCB3';
             tiles[i].style.color = '#BDCCB3';
           }
           else if (tiles[i].innerHTML == 3) {
             tiles[i].style.backgroundColor = '#ECEEDA';
-            tiles[i].style.color = '#546B51';
           }
           else if (tiles[i].innerHTML == 9) {
             tiles[i].style.backgroundColor = '#E0EDC8';
-            tiles[i].style.color = '#546B51';
           }
           else if (tiles[i].innerHTML == 27) {
             tiles[i].style.backgroundColor = '#CFF293';
-            tiles[i].style.color = '#546B51';
           }
           else if (tiles[i].innerHTML == 81) {
             tiles[i].style.backgroundColor = '#9CD539';
-            tiles[i].style.color = '#546B51';
           }
           else if (tiles[i].innerHTML == 243) {
             tiles[i].style.backgroundColor = '#0CB010';

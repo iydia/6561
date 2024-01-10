b6602d388fa17f40dd254867ffa113c7509f9aed
Lydia He <75390984+iydia@users.noreply.github.com>
2024-01-09 20:44:42 -0500

    New style

--- t2/t2.js
+++ t2/t2.js
@@ -185,7 +185,7 @@ document.addEventListener('DOMContentLoaded', () =>  {
     function checkForWin() {
       for (let i=0; i < squares.length; i++) {
         if (squares[i].innerHTML == 2048) {
-          resultDisplay.innerHTML = 'You WIN'
+          resultDisplay.innerHTML = 'WINNER!'
           document.removeEventListener('keyup', control)
           setTimeout(() => clear(), 3000)
         }
@@ -201,7 +201,7 @@ document.addEventListener('DOMContentLoaded', () =>  {
         }
       }
       if (zeros === 0) {
-        resultDisplay.innerHTML = 'You LOSE'
+        resultDisplay.innerHTML = 'GAME OVER'
         document.removeEventListener('keyup', control)
         setTimeout(() => clear(), 3000)
       }
